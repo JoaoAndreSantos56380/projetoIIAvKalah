bec7a59add38416aee5f0f3b515c24756c478f15
56380 <joao.bf4@outlook.com>
2023-11-10 00:44:34 +0000

    debug config

---
+++ debug.py
@@ -0,0 +1,12 @@
+from jogos import *
+from kalah import *
+from main import *
+from tournament import *
+
+
+eu = JogadorAlfaBeta("YO", 4, steal_seeds_better)
+andre7 = Chapiteau( 4, chapiteau)
+
+jogo = Kalah(69)
+print(joga11(jogo, eu, andre7, True))
+
