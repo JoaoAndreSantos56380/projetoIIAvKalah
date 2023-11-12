077c75c3e58e9e628c4ce7b8cb8f4134bc8cd3e0
56380 <joao.bf4@outlook.com>
2023-11-12 02:50:18 +0000

    update debug config

--- debug.py
+++ debug.py
@@ -4,9 +4,14 @@ from main import *
 from tournament import *
 
 
-eu = JogadorAlfaBeta("YO", 4, steal_seeds_better)
-andre7 = Chapiteau( 4, chapiteau)
+eu = JogadorAlfaBeta("YO1", 4, won_seeds_diff)
+eu2 = JogadorAlfaBeta("YO2", 4, won_seeds_diff2)
+eu3 = JogadorAlfaBeta("YO3", 4, won_seeds_diff3)
+ronaldo = JogadorAlfaBeta("roni", 4, teste)
+broken = JogadorAlfaBeta("borken", 4, won_seeds_diff3)
+chap = Chapiteau( "chap", 4)
 
-jogo = Kalah(69)
-print(joga11(jogo, eu, andre7, True))
+jogo = Kalah(80)
+print(joga11(jogo, ronaldo, broken, True))
+#print(joga11(jogo, chap, ronaldo, True))
 
